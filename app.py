e590cb0d99ea610516361db2cf35e8324632c112
Macfeigh <macfeighbitange1@gmail.com>
2026-01-20 09:36:31 +0300

    app.py

--- app.py
+++ app.py
@@ -10,21 +10,33 @@ user_sessions = {}
 
 @app.route("/", methods=['GET'])
 def home():
-    # This tells Python to look for the HTML file in the templates folder
-    return render_template("index.html")
+    # Your specific product data integrated here
+    product_data = {
+        "products": [
+            {"id": 1, "name": "DAP Fertilizer 50kg", "price": "3,500"},
+            {"id": 2, "name": "Cattle Dip 1L", "price": "1,200"},
+            {"id": 3, "name": "Hybrid Maize Seeds 2kg", "price": "800"}
+        ]
+    }
+    # Passing the list of products to index.html
+    return render_template("index.html", products=product_data["products"])
 
 @app.route("/whatsapp", methods=['POST'])
 def whatsapp_reply():
+    # Get details from Twilio request
     sender_id = request.values.get('From')
     incoming_msg = request.values.get('Body', '')
     
+    # Process the message through your logic engine
     reply_text = get_menu_response(sender_id, incoming_msg, user_sessions)
     
+    # Build Twilio TwiML response
     resp = MessagingResponse()
     resp.message(reply_text)
     
     return str(resp)
 
 if __name__ == "__main__":
+    # Get port from environment or default to 10000 for Render
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
